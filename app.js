6f2a3e8fe6710fc16dd8c23e4b4c2bbbafcade3d
Emir Kerim Eskici <86002529+EmirKerimEskici@users.noreply.github.com>
2021-09-13 14:03:59 +0300

    Update

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-$(".inputData").keyup(function (e) {
-    if (e.keyCode == 13) {
-        var data = $(".inputData").val()
+var list = [];
+function showList() {
+    $(".list li").remove();
+    for (var i = 0; i < list.length; i++) {
         $(".list").append(`
-             <li>
-                  <input class='input' type="checkbox" class='btn-bck'>
-                  <label>${data}</label>
-                  <img class="delete-button" src="images/vector.svg" alt="">
-             </li>
-        `);}
-});
-$("body").on("click", ".delete-button", function () {
-    $(this).parent().remove();
+           <li>
+               <input class='input' type="checkbox" class='btn-bck'>
+              <label>${list[i]}</label>
+                <img class="delete-button" src="images/vector.svg" onclick="delData('${list[i]}')">
+            </li>
+       `);
+    }}
+showList();
+$(".inputData").keyup(function (e) {
+    $(".inputData").keyup(function (e) {
+        if (e.keyCode == 13) {
+            if($(".inputData").val() !== ""){
+                list.push($(".inputData").val());
+                $(".inputData").val("");
+                showList();
+            }
+        }
+    });});
+function delData(removeItem) {
+    list = jQuery.grep(list, function(value) {
+        return value != removeItem;
+    });
+    showList();
+}
 
-});
