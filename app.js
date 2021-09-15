314db59bb1694ac65f73eb87e9b662fd5eb3335e
Emir Kerim Eskici <86002529+EmirKerimEskici@users.noreply.github.com>
2021-09-16 00:41:30 +0300

    List

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 var list = [];
-function showList() {
+function pushList() {
     $(".list li").remove();
+    console.log(list)
     for (var i = 0; i < list.length; i++) {
         $(".list").append(`
            <li>
-               <input class='input' type="checkbox" class='btn-bck'>
-              <label>${list[i]}</label>
-                <img class="delete-button" src="images/vector.svg" onclick="delData('${list[i]}')">
+               <input class='input btn-bck' value="${list[i].id}" type="checkbox" >
+              <label>${list[i].todo}</label>
+                <img class="delete-button" src="images/vector.svg" onclick="removeLi(${list[i].id})">
             </li>
-       `);
-    }}
-showList();
-$(".inputData").keyup(function (e) {
-    $(".inputData").keyup(function (e) {
-        if (e.keyCode == 13) {
-            if($(".inputData").val() !== ""){
-                list.push($(".inputData").val());
-                $(".inputData").val("");
-                showList();
-            }
-        }
-    });});
-function delData(removeItem) {
-    list = jQuery.grep(list, function(value) {
-        return value != removeItem;
-    });
-    showList();
+       `);}
 }
+pushList();
+$(".inputData").keyup(function (e) {
+    if (e.keyCode == 13) {
+        if ($(".inputData").val() !== "") {
+            var data = {
+                id: Date.now(),
+                todo: $(".inputData").val(),
+                isCompleted: false
+
+            }
+            list.push(data);
+            $(".inputData").val("");
+            pushList();
+        } }
+});
+$("body").on("change", ".btn-bck", function () {
+    if (this.checked) {
+        list.find(x => x.id === parseInt($(this).val())).isCompleted = true;
+    } else {
+        list.find(x => x.id == parseInt($(this).val())).isCompleted = false;
+    }
+    console.log(list)
+});
+function removeLi(removeItem) {
+    list.splice(list.findIndex(x => x.id === parseInt(removeItem)), 1)
+    pushList();
+}
+
+
+
